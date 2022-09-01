0f35b4f6d4ada2529c7449e7ba401c8b3af5a836
Ken Preiser <kenpreiser@gmail.com>
2022-08-31 17:44:20 -0700

    Create IENS.sol

---
+++ IENS.sol
@@ -0,0 +1,7 @@
+abstract contract ENS {
+    function resolver(bytes32 node) public virtual view returns (Resolver);
+}
+
+abstract contract Resolver {
+    function addr(bytes32 node) public virtual view returns (address);
+}
